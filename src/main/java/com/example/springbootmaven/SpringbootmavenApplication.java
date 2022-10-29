b32387b5ee344c308ac6b3a066be99aa999d969e
dongseoklee <dongseok.lee.log@gmail.com>
2022-10-29 16:14:47 +0900

    Trying to deploying as jar..(current state is fail)

--- src/main/java/com/example/springbootmaven/SpringbootmavenApplication.java
+++ src/main/java/com/example/springbootmaven/SpringbootmavenApplication.java
@@ -1,13 +1,21 @@
 package com.example.springbootmaven;
 
+import com.jam2in.arcus.app.common.config.ArcusConfiguration;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.AutoConfigurationExcludeFilter;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.TypeExcludeFilter;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.*;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Component;
+
+import java.lang.annotation.*;
 
 @SpringBootApplication
-@ImportResource("applicationContext.xml")
+@ImportResource("classpath:applicationContext.xml")
 public class SpringbootmavenApplication {
 
 	public static void main(String[] args) {
--- src/test/java/com/example/springbootmaven/SpringbootmavenApplicationTests.java
+++ src/test/java/com/example/springbootmaven/SpringbootmavenApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.springbootmaven;
 
+import com.jam2in.arcus.app.common.config.ArcusConfiguration;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,7 @@ class SpringbootmavenApplicationTests {
 
 	@Test
 	void contextLoads() {
+
 	}
 
 }
